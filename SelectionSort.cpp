4a4a842d4c1a790fe3e8e061870a82aaab97bc3d
Utkarsh Patel <utkarshp04@gmail.com>
2023-02-18 18:45:01 +0530

    Add files via upload

---
+++ SelectionSort.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {5,6,8,5,2};
+    int min;
+    int temp;
+    int count;
+    for(int i = 0 ; i<4; i++) {
+        min = arr[i];
+        temp = arr[i];
+        count = i;
+        int j = i+1;
+        
+        for(; j<5;j++) {
+            
+            if(arr[j]<min) {
+                min = arr[j];
+                count = j;
+
+            }
+        }
+
+
+        arr[i] = arr[count];
+        arr[count] = temp;
+        //Swapping
+
+
+
+
+    }
+
+    for(int i =0 ; i<5;i++) {
+        cout<<arr[i]<<endl;
+    }
+
+
+}
