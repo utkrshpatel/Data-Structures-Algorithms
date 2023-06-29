a6953ea1b8adcbf586e39b0db5f4353488bff817
Utkarsh Patel <utkarshp04@gmail.com>
2023-06-29 19:44:02 +0530

    Add files via upload

---
+++ setMatrixZeroesleetcode73.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void setZeroes(vector<vector<int>>& matrix) {
+    vector<pair<int, int>> matrixIndices;
+   
+    
+    int rows = matrix[0].size();
+    int col = matrix.size();
+
+
+       for(int i = 0; i<col; i++) {
+        for(int j = 0; j<rows;j++) {
+            if(matrix[i][j] == 0) {
+                matrixIndices.push_back(make_pair(i,j));
+              
+            }
+        }
+       }
+    
+
+
+    for(int i = 0; i<col; i++) {
+        for(int j = 0; j<rows;j++) {
+
+            for(auto mat : matrixIndices) {
+                if(mat.first == i && mat.second == j) {
+                     for(int m = 0; m<rows; m++) {
+                    matrix[i][m] = 0;
+                }
+
+                for(int n = 0; n<col; n++) {
+                    matrix[n][j] = 0;
+
+                }
+
+                break;
+
+                
+            }
+            
+        }
+    }
+    }
+
+}
+
+
+        
+
+
+
+int main() {
+    vector<vector<int>> v{{0,1,2,0},{3,4,5,2},{1,3,1,5}};
+
+
+
+}
