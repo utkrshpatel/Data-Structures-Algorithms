24ace3241688c167552355cb67b3b306c0dc04a1
Utkarsh Patel <utkarshp04@gmail.com>
2023-07-06 21:31:28 +0530

    Add files via upload

---
+++ rotateArrayusingList.cpp
@@ -0,0 +1,50 @@
+//Program to rotate array by k times
+#include <iostream>
+#include <vector>
+#include <list>
+using namespace std;
+
+
+
+
+
+void rotate(vector<int>& nums, int k) {
+    list<int>li;
+
+    for(int i = 0; i<nums.size(); i++) {
+        li.push_back(nums[i]);
+    }
+
+    for(int i = 0; i<k; i++) {
+        int data = li.back();
+        li.pop_back();
+        li.push_front(data);
+    }
+
+    auto it = li.begin();
+
+    for(int i = 0; i<nums.size(); i++) {
+        nums[i] = *it;
+        it++;
+    }
+
+
+    for(int i = 0; i<nums.size(); i++) {
+       cout<<nums[i]<<" ";
+    }
+    
+}
+
+
+
+int main() {
+    vector<int>nums{-1,-100,3,99};
+    int k = 2;
+    
+    rotate(nums,k);
+
+    
+    for(int i = 0; i<nums.size(); i++) {
+       cout<<nums[i]<<" ";
+    }
+}
